a56972439ae0149d92b6665228f61067c79d8adf
Mohit Rajan E <mohit.rajan@bigbinary.com>
2023-11-08 16:33:10 +0530

    update styles and refactor html structure

--- src/components/Bubble.tsx
+++ src/components/Bubble.tsx
@@ -12,56 +12,46 @@ type BubbleProps = {
 } & React.HTMLProps<HTMLDivElement>;
 
 function Bubble({ user, message, isLoading = false }: BubbleProps) {
-  const bubbleRef = useRef<HTMLDivElement | null>(null);
+  // const bubbleRef = useRef<HTMLDivElement | null>(null);
 
   const isUserBot = user?.isBot ?? false;
   const sanitize = DOMPurify.sanitize;
 
   return (
     <div
-      ref={bubbleRef}
-      className={classNames({
-        "chat-bubble__wrapper--user": !isUserBot,
+      className={classNames("chat-bubble", {
+        "chat-bubble--user": !isUserBot,
       })}
     >
       <div
-        className={classNames("sm:mt-6 chat-bubble", {
-          "chat-bubble--user": !isUserBot,
+        className={classNames("chat-bubble__name", {
+          "chat-bubble__name--bot": isUserBot,
         })}
       >
+        {user?.name[0]?.toUpperCase()}
+      </div>
+      <div>
         <div
-          className={classNames("chat-bubble__name", {
-            "chat-bubble__name--bot": isUserBot,
+          className={classNames("chat-bubble__message--wrapper", {
+            "chat-bubble__message--wrapper--user": !isUserBot,
+
+            "chat-bubble__message--wrapper--bot": isUserBot,
           })}
         >
-          {user?.name[0]?.toUpperCase()}
-        </div>
-        <div>
-          <div
-            className={classNames("chat-bubble__message--wrapper", {
-              "chat-bubble__message--wrapper--user": !isUserBot,
-
-              "chat-bubble__message--wrapper--bot": isUserBot,
-            })}
-          >
-            {isLoading ? (
-              <div className="typing">
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-              </div>
-            ) : (
-              <div
-                className="chat-bubble__message"
-                dangerouslySetInnerHTML={{
-                  __html: sanitize(message || ""),
-                }}
-              />
-            )}
-            <div className="media-body">
-              <div className="msg-box"></div>
+          {isLoading ? (
+            <div className="typing">
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
             </div>
-          </div>
+          ) : (
+            <div
+              className="chat-bubble__message"
+              dangerouslySetInnerHTML={{
+                __html: sanitize(message || ""),
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
