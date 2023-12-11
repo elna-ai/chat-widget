ecc6b2017b1613ff4a43f71bfde8b4eefd7c4575
Mohit Rajan E <mohit.rajan@bigbinary.com>
2023-12-11 09:58:45 +0530

    make textarea auto resiziable

---
+++ src/hooks/useAutoResizeTextArea.ts
@@ -0,0 +1,28 @@
+import { useLayoutEffect } from "react";
+
+const MIN_TEXT_AREA_HEIGHT=40;
+const MAX_TEXT_AREA_HEIGHT=160;
+
+const useAutoSizeTextArea = (
+  textAreaRef: HTMLTextAreaElement | null,
+  text: string
+) => {
+  useLayoutEffect(() => {
+    if (!textAreaRef) return;
+    
+
+    textAreaRef.style.height = `inherit`;
+    console.log(textAreaRef.scrollHeight);
+    const newHeight = Math.min(
+      Math.max(textAreaRef.scrollHeight ,MIN_TEXT_AREA_HEIGHT ),
+      MAX_TEXT_AREA_HEIGHT
+    );
+    
+    textAreaRef.style.overflowY = newHeight >= MAX_TEXT_AREA_HEIGHT ? "scroll" : "hidden";
+
+    textAreaRef.style.height = `${newHeight}px`;
+  }, [text, textAreaRef]);
+};
+
+
+export default useAutoSizeTextArea;
